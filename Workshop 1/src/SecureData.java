593de1901762859bc2236e6931824a23937d5661
asabagh <asabagh@myseneca.ca>
2021-09-26 23:26:39 -0400

    Update SecureData.java

--- Workshop 1/src/SecureData.java
+++ Workshop 1/src/SecureData.java
@@ -1,3 +1,13 @@
+/**********************************************
+Workshop #1
+Course:JAC444 - Fall 2021
+Name: Amirhossein Sabagh
+ID:152956199
+Section:NDD
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Date:2021-09-26
+**********************************************/
+
 import java.util.Scanner;
 
 public class SecureData {
