9600afa347cf452468eaabcb4718c469710c981f
asabagh <asabagh@myseneca.ca>
2021-09-26 23:26:41 -0400

    Update Craps.java

--- Workshop 1/src/Craps.java
+++ Workshop 1/src/Craps.java
@@ -1,8 +1,22 @@
+/**********************************************
+Workshop #1
+Course:JAC444 - Fall 2021
+Name: Amirhossein Sabagh
+ID:152956199
+Section:NDD
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Date:2021-09-26
+**********************************************/
+
 import java.util.Scanner;
 
 public class Craps {
 	static int die1, die2, round;
 	
+	/**
+	 * This method rolls the dice (generates a random number between 1 - 6)
+	 * @return rolledNum -> returns the randomly generated number
+	 */
 	public static int roll() {
 		int rolledNum = (int)((Math.random() * 6) + 1);
 		 //die1 = (int)((Math.random() * 6) + 1);
@@ -10,12 +24,19 @@ public class Craps {
 		return rolledNum;
 	}
 	
+	/**
+	 * This method prompts the user to continue the program by pressing the Enter key
+	 */
 	public void promptEnterKey(){
 		   System.out.println("Press \"ENTER\" to continue...");
 		   Scanner scanner = new Scanner(System.in);
 		   scanner.nextLine();
 	}
 	
+	/**
+	 * main method: Advanced Craps Game
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		int sum, sum2;
 		Craps c = new Craps();
