7b14c1dc71b43f56e61b0a034a02ddadc1100b9f
asabagh <asabagh@myseneca.ca>
2021-09-27 22:58:49 -0400

    Optimize the application - improve the documentation of the methods

--- Workshop 1/src/AdvancedCraps.java
+++ Workshop 1/src/AdvancedCraps.java
@@ -17,6 +17,23 @@ public class AdvancedCraps {
 	static Scanner scan = new Scanner(System.in);
 	
 	/**
+	 * main method: Advanced Craps Game
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("****** Welcome to craps! ******\n\n");
+		while(!exit) {
+			playCraps();
+			do {
+				again = playAgain();
+				if(again == true) {
+					playCraps();
+				}
+			} while(again == true && !exit);
+		}
+	}	
+	
+	/**
 	 * This method rolls the dice (generates a random number between 1 - 6)
 	 * @return rolledNum -> returns the randomly generated number
 	 */
@@ -36,6 +53,11 @@ public class AdvancedCraps {
 		scan.nextLine();
 	}
 	
+	/**
+	 * This method prompts the user if they want to play again
+	 * @return again -> returns a boolean value that determines
+	 * whether the user wants to play again or wants to leave.
+	 */
 	public static boolean playAgain() {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Do you want to play again?\n1. Yes\n2. No\nSelection: ");
@@ -55,97 +77,11 @@ public class AdvancedCraps {
 			exit = true;
 		}
 		return again;
-	}
-	
-	/*public static void playCraps() {
-		Craps c = new Craps();
-		Scanner scan = new Scanner(System.in);
-		while(bankBalance > 0) {
-			System.out.println("Your balance is: " + bankBalance);
-			System.out.print("Enter a wager: ");
-			double wager = scan.nextDouble();
-			if(wager > bankBalance) {
-				System.out.println("Sorry, you do not have enough money to afford the wager.");
-			}
-			else {
-				if(wager == bankBalance) {
-					System.out.println("Ohhh nice! You've got courage to go all in.");
-				}
-				else if(wager > ((bankBalance * 2) / 3)) {
-					System.out.println("Well! I see someone has got a winner's attitude!");
-				}
-		int sum, sum2;
-		System.out.println("Shall we roll?");
-		c.promptEnterKey();
-		System.out.println("Round #" + ++round);
-		die1 = roll();
-		System.out.println("Die #1 ----> " + die1);
-		die2 = roll();
-		System.out.println("Die #2 ----> " + die2);
-		sum = die1 + die2;
-		if (sum == 7 || sum == 11) {
-			System.out.println("Sum: " + sum);
-			System.out.println("Congratulations You Won!");
-			bankBalance += wager;
-			System.out.println("Your current balance is: " + bankBalance);
-		} 
-		else if (sum == 2 || sum == 3 || sum == 12) {
-			System.out.println("Sum: " + sum);
-		    System.out.println("Craps, Better Luck Next Time, You Lost!");
-		    bankBalance -= wager;
-			System.out.println("Your current balance is: " + bankBalance);
-			if(bankBalance <= 0) {
-				System.out.println("Sorry, You Got Busted!\nCome back later with more cash.");
-				return;
-			}
-		}
-		else {
-			System.out.println("Point is (established) set to " + sum );
-			c.promptEnterKey();
-			do {
-				die1 = roll();
-				die2 = roll();
-				System.out.println("Round #" + ++round + "\nDie #1 ----> " + die1 + "\nDie #2 ----> " + die2);
-				sum2 = die1 + die2;
-				if(sum2 == sum) {
-					System.out.println("Sum: " + sum2);
-					System.out.println("Congratulations, You Won!");
-					bankBalance += wager;
-					System.out.println("Your current balance is: " + bankBalance);
-					break;
-				}
-				else if(sum2 == 7) {
-					System.out.println("Craps, Better Luck Next Time, You Lost!");
-					bankBalance -= wager;
-					System.out.println("Your current balance is: " + bankBalance);
-					if(bankBalance <= 0) {
-						System.out.println("Sorry You Got Busted!\nCome back later with more cash.");
-						return;
-					}
-					break;
-				}
-				c.promptEnterKey();
-			} while(true); //while(sum2 != sum || sum2 != 7);
-	}*/
-	
-	/**
-	 * main method: Advanced Craps Game
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println("****** Welcome to craps! ******\n\n");
-		while(!exit) {
-			playCraps();
-			do {
-				again = playAgain();
-				if(again == true) {
-					playCraps();
-				}
-			} while(again == true && !exit);
-		}
-	}	
-		
+	}		
 
+	/**
+	 * This method plays the game of Craps.
+	 */
 	public static void playCraps() {
 		int sum, sum2;
 		Craps c = new Craps();
@@ -221,6 +157,5 @@ public class AdvancedCraps {
 					} while(!exit); //while(sum2 != sum || sum2 != 7);
 				}
 			}
-		//}	
 	}
 }
