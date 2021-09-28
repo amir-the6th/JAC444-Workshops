b0ac00e3b9c90cd55f573e3e037dc0d51689f101
asabagh <asabagh@myseneca.ca>
2021-09-27 22:24:36 -0400

    Optimize the application

--- Workshop 1/src/Craps.java
+++ Workshop 1/src/Craps.java
@@ -14,27 +14,7 @@ public class Craps {
 	static int die1, die2, round;
 	
 	/**
-	 * This method rolls the dice (generates a random number between 1 - 6)
-	 * @return rolledNum -> returns the randomly generated number
-	 */
-	public static int roll() {
-		int rolledNum = (int)((Math.random() * 6) + 1);
-		 //die1 = (int)((Math.random() * 6) + 1);
-		 //die2 = (int)((Math.random() * 6) + 1);
-		return rolledNum;
-	}
-	
-	/**
-	 * This method prompts the user to continue the program by pressing the Enter key
-	 */
-	public void promptEnterKey(){
-		   System.out.println("Press \"ENTER\" to continue...");
-		   Scanner scanner = new Scanner(System.in);
-		   scanner.nextLine();
-	}
-	
-	/**
-	 * main method: Advanced Craps Game
+	 * main method: Craps Game
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -59,22 +39,42 @@ public class Craps {
 		else {
 			System.out.println("Point is (established) set to " + sum );
 			c.promptEnterKey();
+			do {
+				die1 = roll();
+				die2 = roll();
+				System.out.println("Round #" + ++round + "\nDie #1 ----> " + die1 + "\nDie #2 ----> " + die2);
+				sum2 = die1 + die2;
+				if(sum2 == sum) {
+					System.out.println("Sum: " + sum2);
+					System.out.println("Congratulations, You Won!");
+					break;
+				}
+				else if(sum2 == 7) {
+					System.out.println("Craps, Better Luck Next Time, You Lost!");
+					break;
+				}
+				c.promptEnterKey();
+				} while(true); //while(sum2 != sum || sum2 != 7);
 		}
-		do {
-		die1 = roll();
-		die2 = roll();
-		System.out.println("Round #" + ++round + "\nDie #1 ----> " + die1 + "\nDie #2 ----> " + die2);
-		sum2 = die1 + die2;
-		if(sum2 == sum) {
-			System.out.println("Sum: " + sum2);
-			System.out.println("Congratulations, You Won!");
-			break;
-		}
-		else if(sum2 == 7) {
-			System.out.println("Craps, Better Luck Next Time, You Lost!");
-			break;
-		}
-		c.promptEnterKey();
-		} while(true); //while(sum2 != sum || sum2 != 7);
+	}
+	
+	/**
+	 * This method rolls the dice (generates a random number between 1 - 6)
+	 * @return rolledNum -> returns the randomly generated number
+	 */
+	public static int roll() {
+		int rolledNum = (int)((Math.random() * 6) + 1);
+		 //die1 = (int)((Math.random() * 6) + 1);
+		 //die2 = (int)((Math.random() * 6) + 1);
+		return rolledNum;
+	}
+	
+	/**
+	 * This method prompts the user to continue the program by pressing the Enter key
+	 */
+	public void promptEnterKey(){
+		   System.out.println("Press \"ENTER\" to continue...");
+		   Scanner scanner = new Scanner(System.in);
+		   scanner.nextLine();
 	}
 }
