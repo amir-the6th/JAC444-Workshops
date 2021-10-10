01286f7cfdf602b5fcde9c260e3d0cd772f424c4
asabagh <asabagh@myseneca.ca>
2021-10-10 02:18:59 -0400

    Complete menu options and fix constructor calls
    Add displayDates - display the specified date in various formats

--- Workshop 3/src/DateProgram.java
+++ Workshop 3/src/DateProgram.java
@@ -2,23 +2,43 @@ import java.util.Scanner;
 
 public class DateProgram {
 	public static void main(String[] args) {
+		int dd, mm, yyyy, dOfYear;
+		String mName;
 		Scanner scan = new Scanner(System.in);
+		welcomeHeader();
 		mainMenu();
 		int ch = scan.nextInt();
 		switch(ch) {
 		case 1:
-			System.out.println("Enter Month(1-12): ");
-			int mm = scan.nextInt();
-			System.out.println("Enter Day of Month: ");
-			int dd = scan.nextInt();
-			System.out.println("Enter Year: ");
-			int yyyy = scan.nextInt();
-			Date date = new Date(dd,mm,yyyy);
-			displayDates(date);
+			System.out.print("Enter Month(1-12): ");
+			mm = scan.nextInt();
+			System.out.print("Enter Day of Month: ");
+			dd = scan.nextInt();
+			System.out.print("Enter Year: ");
+			yyyy = scan.nextInt();
+			Date date1 = new Date(dd,mm,yyyy);
+			scan.nextLine();
+			displayDates(date1);
 			break;
 		case 2:
+			System.out.print("Enter Month(January - December): ");
+			mName = scan.next();
+			System.out.print("Enter Day of Month: ");
+			dd = scan.nextInt();
+			System.out.print("Enter Year: ");
+			yyyy = scan.nextInt();
+			Date date2 = new Date(mName,dd,yyyy);
+			scan.nextLine();
+			displayDates(date2);
 			break;
 		case 3:
+			System.out.print("Enter Day of Year: ");
+			dOfYear = scan.nextInt();
+			System.out.print("Enter Year: ");
+			yyyy = scan.nextInt();
+			Date date3 = new Date(dOfYear,yyyy);
+			scan.nextLine();
+			displayDates(date3);
 			break;
 		case 4:
 			System.out.println("Thank you for using our program. Goodbye!");
@@ -36,7 +56,7 @@ public class DateProgram {
 	 */
 	public static void welcomeHeader() {
 		System.out.println("Welcome to Amir's date conversion program!");
-		System.out.print("--------------------------------------------");
+		System.out.println("------------------------------------------");
 	}
 	public static void mainMenu() {
 		System.out.println("Enter 1 for format: MM/DD/YYYY");
@@ -45,9 +65,9 @@ public class DateProgram {
 		System.out.println("Enter 4 for exit");
 		System.out.print("Selection: ");
 	}
-	public static void displayDates(Date date) {
-		System.out.println("MM/DD/YYYY:      ");
-		System.out.println("Month DD, YYYY:  ");
-		System.out.println("DDD YYYY:        ");
+	public static void displayDates(Date d) {
+		System.out.println("MM/DD/YYYY:      " + d.getMonth() + "/" + d.getDay() + "/" + d.getYear());
+		System.out.println("Month DD, YYYY:  " + d.getMonthName() + " " + d.getDay() + ", " + d.getYear());
+		System.out.println("DDD YYYY:        " + d.getDayOfYear() + " " + d.getYear());
 	}
 }
