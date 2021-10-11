290c45ac54d90b00df44008e0f98d9599b4af204
asabagh <asabagh@myseneca.ca>
2021-10-10 23:37:54 -0400

    Add signature and methods documentation

--- Workshop 3/src/Date.java
+++ Workshop 3/src/Date.java
@@ -1,3 +1,12 @@
+/**********************************************
+Workshop #3
+Course:JAC444 - Fall 2021
+Name: Amirhossein Sabagh
+ID:152956199
+Section:NDD
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Date:2021-10-10
+**********************************************/
 import java.util.Calendar;
 import java.util.Locale;
 import java.time.LocalDate;
@@ -8,6 +17,12 @@ import java.time.format.TextStyle;
 public class Date {
 	private int dd, mm, yyyy, dayOfYear;
 	private String monthName;
+	/**
+	 * 3-arg constructor
+	 * @param day - int
+	 * @param month - int
+	 * @param year - int
+	 */
 	public Date(int day, int month, int year){
 		Calendar cal = Calendar.getInstance();
 		dd = day;
@@ -19,6 +34,12 @@ public class Date {
 		cal.set(year, month-1, day);
 		dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
 	}
+	/**
+	 * 3-arg constructor
+	 * @param month - String
+	 * @param day - int
+	 * @param year - int
+	 */
 	public Date(String month, int day, int year) {
 		Calendar cal = Calendar.getInstance();
 		dd = day;
@@ -28,6 +49,11 @@ public class Date {
 		cal.set(year, mm-1, day);
 		dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
 	}
+	/**
+	 * 2-arg constructor
+	 * @param day - int
+	 * @param year - int
+	 */
 	public Date(int day, int year) {
 		LocalDate ld = Year.of( year ).atDay( day ) ;
         dd = ld.getDayOfMonth();
@@ -38,18 +64,33 @@ public class Date {
 				Locale.CANADA.ENGLISH);
         dayOfYear = day;
 	}
+	/**
+	 * @return dd - day number
+	 */
 	public int getDay() {
 		return dd;
 	}
+	/**
+	 * @return mm - month number
+	 */
 	public int getMonth() {
 		return mm;
 	}
+	/**
+	 * @return yyyy - year number
+	 */
 	public int getYear() {
 		return yyyy;
 	}
+	/**
+	 * @return dayOfYear - day number (of year)
+	 */
 	public int getDayOfYear() {
 		return dayOfYear;
 	}
+	/**
+	 * @return monthName - month name
+	 */
 	public String getMonthName() {
 		return monthName;
 	}
