9408832e1c561d9a60790b15933d1aa4910e66d3
asabagh <asabagh@myseneca.ca>
2021-10-17 18:20:14 -0400

    Implement necessary methods

--- Workshop 4/src/ca/senecacollege/concrete/SalariedEmployee.java
+++ Workshop 4/src/ca/senecacollege/concrete/SalariedEmployee.java
@@ -12,16 +12,63 @@ package ca.senecacollege.concrete;
 import ca.senecacollege.abstracts.Employee;
 
 /**
- * @author amirs
+ * Class: SalariedEmployee
+ * Extends Employee abstract class
+ * @author Amir
  *
  */
 public class SalariedEmployee extends Employee {
+	double weeklySalary;
 
 	/**
-	 * 
+	 * Four-arg constructor
+	 * calls superclass constructor first.
+	 * then initializes instance variables.
+	 * @param firstName
+	 * @param lastName
+	 * @param socialSecurityNumber
+	 * @param weeklySalary
+	 * @throws Exception 
 	 */
-	public SalariedEmployee() {
-		// TODO Auto-generated constructor stub
+	public SalariedEmployee(String fName, 
+			String lName, 
+			String ssn, 
+			double ws) throws Exception {
+		super(fName,lName,ssn);
+		setWeeklySalary(ws);
+	}
+	
+	/**
+	 * Getter
+	 * @return weeklySalary
+	 */
+	public double getWeeklySalary() {
+		return this.weeklySalary;
+	}
+	
+	/**
+	 * Setter
+	 * Sets weeklySalary
+	 */
+	public void setWeeklySalary(double ws) throws Exception {
+		if (ws <= 0.0) {
+			throw new Exception("Weekly Salary should not be less than 0.0!");
+		} else {
+			this.weeklySalary = ws;
+		}
+	}
+	
+	/**
+	 * @return String representation of SalariedEmployee Object
+	 */
+	@Override
+	public String toString() {
+		return super.toString() + "\n Salaried Employee [Weekly Salary: $" + getWeeklySalary() + "]";
+	}
+	
+	@Override
+	public double getPaymentAmount() {
+		return getWeeklySalary();
 	}
 
 }
