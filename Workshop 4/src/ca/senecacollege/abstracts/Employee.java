3e33f020c9486827ece00f83fd0d7e1317c382a9
asabagh <asabagh@myseneca.ca>
2021-10-17 16:13:25 -0400

    Implement necessary methods

--- Workshop 4/src/ca/senecacollege/abstracts/Employee.java
+++ Workshop 4/src/ca/senecacollege/abstracts/Employee.java
@@ -12,14 +12,62 @@ package ca.senecacollege.abstracts;
 import ca.senecacollege.interfaces.Payable;
 
 /**
- * @author amirs
+ * Class: Employee
+ * Implement Payable interface
+ * @author Amir
  *
  */
 public abstract class Employee implements Payable {
-
-	@Override
-	public double getPaymentAmount() {
-		// TODO Auto-generated method stub
-		return 0;
+	private String firstName, lastName, socialSecurityNumber;
+	
+	/**
+	 * Three-arg constructor
+	 * @param firstName
+	 * @param lastName
+	 * @param socialSecurityNumber
+	 */
+	public Employee(String fName, String lName, String ssn) {
+		this.firstName = fName;
+		this.lastName = lName;
+		this.socialSecurityNumber = ssn;
 	}
+
+	/**
+	 * Getters
+	 * @return firstName, lastName, socialSecurityNumber
+	 */
+	public String getFName() {
+		return this.firstName;
+	}
+	public String getLName() {
+		return this.lastName;
+	}
+	public String getSSN() {
+		return this.socialSecurityNumber;
+	}
+	
+	/**
+	 * Setters
+	 * Sets firstName, lastName, socialSecurityNumber
+	 */
+	public void setFName(String str) {
+		this.firstName = str;
+	}
+	public void setLName(String str) {
+		this.lastName = str;
+	}
+	public void setSSN(String str) {
+		this.socialSecurityNumber = str;
+	}
+	
+	/**
+	 * @return String representation of Invoice Object
+	 */
+	public String toString() {
+		return "Employee [First Name= " + firstName + 
+				  "/ Last Name= " + lastName + 
+				  "/ Social Security Number= " + socialSecurityNumber + 
+				  "]";
+	}
+	
 }
