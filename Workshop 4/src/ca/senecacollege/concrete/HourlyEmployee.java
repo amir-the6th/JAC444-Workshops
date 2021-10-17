8380f2747c5c8767cec1bf16149cf80d62a711b2
asabagh <asabagh@myseneca.ca>
2021-10-17 18:15:13 -0400

    Implement necessary methods

--- Workshop 4/src/ca/senecacollege/concrete/HourlyEmployee.java
+++ Workshop 4/src/ca/senecacollege/concrete/HourlyEmployee.java
@@ -12,16 +12,85 @@ package ca.senecacollege.concrete;
 import ca.senecacollege.abstracts.Employee;
 
 /**
- * @author amirs
+ * Class: HourlyEmployee
+ * Extends Employee abstract class
+ * @author Amir
  *
  */
 public class HourlyEmployee extends Employee {
+	double wage, hours;
 
 	/**
-	 * 
+	 * Five-arg constructor
+	 * calls superclass constructor first.
+	 * then initializes instance variables.
+	 * @param firstName
+	 * @param lastName
+	 * @param socialSecurityNumber
+	 * @param wage
+	 * @param hours
+	 * @throws Exception 
 	 */
-	public HourlyEmployee() {
-		// TODO Auto-generated constructor stub
+	public HourlyEmployee(String fName, 
+			String lName, 
+			String ssn, 
+			double wage, 
+			double hours) throws Exception {
+		super(fName,lName,ssn);
+		setWage(wage);
+		setHours(hours);
+	}
+	
+	/**
+	 * Getters
+	 * @return wage, hours
+	 */
+	public double getWage() {
+		return this.wage;
+	}
+	public double getHours() {
+		return this.hours;
+	}
+	
+	/**
+	 * Setters
+	 * Sets wage, hours
+	 */
+	public void setWage(double w) throws Exception {
+		if (w <= 0.0) {
+			throw new Exception("Wage should not be less than 0.0!");
+		} else {
+			this.wage = w;
+		}
+	}
+	public void setHours(double h) throws Exception {
+		if ((h <= 0.0) || (h >= 168.0)) {
+			throw new Exception("Hours should not be between 0.0 and 168.0!");
+		} else {
+			this.hours = h;
+		}
+	}
+	
+	/**
+	 * Calculate the hourly based salary.
+	 * @return salary
+	 */
+	@Override
+	public double getPaymentAmount() {
+		double salary = 0.0;
+		// Logic: calculate 40 hours with normal wage. add overtime
+		//        pay for all hours worked in excess of 40 hours.
+		salary = (40 * getWage()) + ((getHours() - 40) * (getWage() * 1.5));		
+		return salary;
+	}
+	
+	/**
+	 * @return String representation of HourlyEmployee Object
+	 */
+	@Override
+	public String toString() {
+		return super.toString() + "\n Hourly Employee [Wage: $" + getWage() + 
+				                "/ Hours: " + getHours() + "]";
 	}
 
 }
