e1a5c13ed2abe0702f74a07644080502ee0988b7
asabagh <asabagh@myseneca.ca>
2021-10-17 15:27:23 -0400

    Add Signature

--- Workshop 4/src/ca/senecacollege/interfaces/Payable.java
+++ Workshop 4/src/ca/senecacollege/interfaces/Payable.java
@@ -1,6 +1,12 @@
-/**
- * 
- */
+/**********************************************
+Workshop #4
+Course:JAC444 - Fall 2021
+Name: Amirhossein Sabagh
+ID:152956199
+Section:NDD
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Date:2021-10-17
+**********************************************/
 package ca.senecacollege.interfaces;
 
 /**
