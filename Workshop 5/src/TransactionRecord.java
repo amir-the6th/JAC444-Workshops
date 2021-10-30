999d42213f4b21992e9ed049caa3b188fae57521
asabagh <asabagh@myseneca.ca>
2021-10-29 21:51:50 -0400

    Create TranscationRecord class and add methods

---
+++ Workshop 5/src/TransactionRecord.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * @author amirs
+ *
+ */
+public class TransactionRecord {
+	
+	private int accountNumber;
+	private double amount;
+
+	/**
+	 * 
+	 */
+	public TransactionRecord() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getAccNum() {
+		return accountNumber;
+	}
+	public void setAccNum(int num) {
+		accountNumber = num;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double num) {
+		amount = num;
+	}
+
+}
