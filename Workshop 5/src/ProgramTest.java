81765fdcb01a32fbd8c3bc1456e4924c0ab5c2de
asabagh <asabagh@myseneca.ca>
2021-10-31 23:57:09 -0400

    Create the main method and test the program

---
+++ Workshop 5/src/ProgramTest.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+/**
+ * 
+ */
+
+/**
+ * @author amirs
+ *
+ */
+public class ProgramTest {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		CreateData cd = new CreateData();
+		FileMatch  fm = new FileMatch();
+	}
+
+}
