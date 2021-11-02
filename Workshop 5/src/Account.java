8d5fae90bc0157e69adae43aace4df41be36bdf0
asabagh <asabagh@myseneca.ca>
2021-11-01 22:21:33 -0400

    Add signature

--- Workshop 5/src/Account.java
+++ Workshop 5/src/Account.java
@@ -1,4 +1,17 @@
+/**********************************************
+Workshop #5
+Course:JAC444 - Fall 2021
+Name: Amirhossein Sabagh
+ID:152956199
+Section:NDD
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Date:2021-11-01
+**********************************************/
 
+/**
+ * @author Amir
+ *
+ */
 public class Account {
 
 	private int accNum;
