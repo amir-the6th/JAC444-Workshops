a8503443e143150300ce6831dcf40938bb5b118e
asabagh <asabagh@myseneca.ca>
2021-11-17 20:58:36 -0500

    Create TestArrayProcessor.java

---
+++ Workshop 7/src/ca/senecacollege/task2/TestArrayProcessor.java
@@ -0,0 +1,14 @@
+package ca.senecacollege.task2;
+
+public class TestArrayProcessor {
+
+	public TestArrayProcessor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
