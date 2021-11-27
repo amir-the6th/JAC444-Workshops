a0155c67c64958f12f7e67604c8a1752d306d3a9
asabagh <asabagh@myseneca.ca>
2021-11-26 23:51:22 -0500

    Create a JavaFX GUI for countries & capital application

--- Workshop 8/src/CountriesCapitals.java
+++ Workshop 8/src/CountriesCapitals.java
@@ -1,27 +1,77 @@
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class CountriesCapitals {
+public class CountriesCapitals extends Application {
 
 	public static void main(String[] args) {
-		boolean exit = false;
-		Scanner scan = new Scanner(System.in);
-		
-		Map<String, String> countriesCapitals = setData();
-		
-		while(!exit) {
-			System.out.print("Enter a country: ");
-			String country = scan.nextLine();
-			
-			if (countriesCapitals.get(country) != null) {
-				System.out.println("The capital of " + country + " is " 
-					+ countriesCapitals.get(country) + "\n");
-			}
-		}
-		scan.close();
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage ps) throws Exception {
+		ps.setTitle("Countries & Capitals");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Text sceneTitle = new Text("Welcome To Countries & Capitals Application");
+        sceneTitle.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+        
+        Label countryLabel = new Label("Enter A Country");
+        grid.add(countryLabel, 0, 1);
+        
+        final TextField country = new TextField();
+        country.setPrefColumnCount(7);
+        country.getText();
+        GridPane.setConstraints(country, 1, 1);
+        grid.getChildren().add(country);
+        
+        //Check Guessed Number Button
+        Button checkBtn = new Button("Check");
+        GridPane.setConstraints(checkBtn, 0, 3);
+        grid.getChildren().add(checkBtn); 
+        
+        Label capital = new Label("");
+        capital.setFont(Font.font("Helvetica", FontWeight.BOLD, 15));
+        capital.setTextFill(Color.BLUE);
+        grid.add(capital, 0, 0, 5, 24);
+        
+        //Check-capital-for-a-country Button Event Handler
+        checkBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				String countryText = country.getText();
+				Map<String, String> countriesCapitals = setData();
+				if (countriesCapitals.get(countryText) != null) {
+					capital.setText("The capital of " + countryText + " is " 
+						+ countriesCapitals.get(countryText) + "\n");
+				}
+			}   	
+        });
+        
+        Scene scene = new Scene(grid, 600, 400);
+        ps.setScene(scene);
+        ps.show();
+	}
+	
 	public static Map<String, String> setData() {
 		Map<String, String> map = new HashMap<>();
 		String[][] countriesData = {
@@ -51,9 +101,7 @@ public class CountriesCapitals {
 				{"Italy", "Rome"},
 				{"Japan", "Tokyo"},
 		};
-		
 		for (int i=0; i < countriesData.length; i++) map.put(countriesData[i][0], countriesData[i][1]);
-		
 		return map;
 	}
 }
