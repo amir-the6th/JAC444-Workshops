b996360f384734554c5fb8142e3c02da35c8277c
asabagh <asabagh@myseneca.ca>
2021-12-01 21:22:54 -0500

    Create test class - create threads to test parallel multiply and compare the execution time with normal call

---
+++ Workshop 9/src/task1/TestMultiplication.java
@@ -0,0 +1,43 @@
+package task1;
+
+public class TestMultiplication {
+	final static int SIZE = 2000;
+	static long beginTime = System.currentTimeMillis(), stopTime;
+	static double[][] a = new double[SIZE][SIZE];
+	static double[][] b = new double[SIZE][SIZE];
+	static double[][] res;
+	
+	public static void main(String[] args) {		
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < b[i].length; j++) {
+				a[i][j] = Math.random() * 1000;
+				b[i][j] = Math.random() * 1000;
+			}
+		}
+		
+		// parallel method
+		System.out.println("Starting Parallel Thread!");
+		Thread t1 = new Thread(new MatrixMultiplication(a, b, 0, SIZE/4));
+		Thread t2 = new Thread(new MatrixMultiplication(a, b, SIZE/4, (SIZE*2)/4));
+		Thread t3 = new Thread(new MatrixMultiplication(a, b, (SIZE*2)/4, (SIZE*3)/4));
+		Thread t4 = new Thread(new MatrixMultiplication(a, b, (SIZE*3)/4, SIZE));
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+			t4.join();
+		} catch (InterruptedException e) { e.printStackTrace(); };	
+		stopTime = System.currentTimeMillis();
+		System.out.println("The execution time for adding the matrices together using threading: " + (stopTime-beginTime) + " milliseconds");
+		
+		// sequential method
+		System.out.println("\nStarting Sequential Method!");
+		MatrixMultiplication.sequentialAddMatrix(a, b);
+		stopTime = System.currentTimeMillis();
+		System.out.println("The execution time for adding the matrices together using sequential call: " + (stopTime-beginTime) + " milliseconds");
+	}
+}
